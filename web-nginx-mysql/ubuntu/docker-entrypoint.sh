8f2cbd39481a13a5597d85b725ab53ea24ba8ef1
Alexey Pustovalov <alexey.pustovalov@zabbix.com>
2020-04-28 22:16:47 +0300

    Updated Ubuntu images

--- web-nginx-mysql/ubuntu/docker-entrypoint.sh
+++ web-nginx-mysql/ubuntu/docker-entrypoint.sh
@@ -237,7 +237,7 @@ prepare_zbx_web_config() {
 
     ZBX_WEB_CONFIG="$ZABBIX_ETC_DIR/web/zabbix.conf.php"
 
-    PHP_CONFIG_FILE="/etc/php/7.2/apache2/conf.d/99-zabbix.ini"
+    PHP_CONFIG_FILE="/etc/php/7.2/fpm/pool.d/zabbix.conf"
 
     update_config_var "$PHP_CONFIG_FILE" "php_value[max_execution_time]" "${ZBX_MAXEXECUTIONTIME:-"600"}"
     update_config_var "$PHP_CONFIG_FILE" "php_value[memory_limit]" "${ZBX_MEMORYLIMIT:-"128M"}"
