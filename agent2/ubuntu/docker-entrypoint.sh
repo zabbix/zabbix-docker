0bf2a20d6c324954759652e15bca7e54206236cd
Alexey Pustovalov <alexey.pustovalov@zabbix.com>
2021-04-10 06:05:46 -0400

    Added Zabbix agent 2 Ubuntu image

---
+++ agent2/ubuntu/docker-entrypoint.sh
@@ -0,0 +1,217 @@
+#!/bin/bash
+
+set -o pipefail
+
+set +e
+
+# Script trace mode
+if [ "${DEBUG_MODE}" == "true" ]; then
+    set -o xtrace
+fi
+
+# Default Zabbix installation name
+# Default Zabbix server host
+: ${ZBX_SERVER_HOST:="zabbix-server"}
+# Default Zabbix server port number
+: ${ZBX_SERVER_PORT:="10051"}
+
+# Default directories
+# User 'zabbix' home directory
+ZABBIX_USER_HOME_DIR="/var/lib/zabbix"
+# Configuration files directory
+ZABBIX_ETC_DIR="/etc/zabbix"
+
+escape_spec_char() {
+    local var_value=$1
+
+    var_value="${var_value//\\/\\\\}"
+    var_value="${var_value//[$'\n']/}"
+    var_value="${var_value//\//\\/}"
+    var_value="${var_value//./\\.}"
+    var_value="${var_value//\*/\\*}"
+    var_value="${var_value//^/\\^}"
+    var_value="${var_value//\$/\\\$}"
+    var_value="${var_value//\&/\\\&}"
+    var_value="${var_value//\[/\\[}"
+    var_value="${var_value//\]/\\]}"
+
+    echo "$var_value"
+}
+
+update_config_var() {
+    local config_path=$1
+    local var_name=$2
+    local var_value=$3
+    local is_multiple=$4
+
+    local masklist=("TLSPSKIdentity")
+
+    if [ ! -f "$config_path" ]; then
+        echo "**** Configuration file '$config_path' does not exist"
+        return
+    fi
+
+    if [[ " ${masklist[@]} " =~ " $var_name " ]] && [ ! -z "$var_value" ]; then
+        echo -n "** Updating '$config_path' parameter \"$var_name\": '****'. Enable DEBUG_MODE to view value ..."
+    else
+        echo -n "** Updating '$config_path' parameter \"$var_name\": '$var_value'..."
+    fi
+
+    # Remove configuration parameter definition in case of unset parameter value
+    if [ -z "$var_value" ]; then
+        sed -i -e "/^$var_name=/d" "$config_path"
+        echo "removed"
+        return
+    fi
+
+    # Remove value from configuration parameter in case of double quoted parameter value
+    if [ "$var_value" == '""' ]; then
+        sed -i -e "/^$var_name=/s/=.*/=/" "$config_path"
+        echo "undefined"
+        return
+    fi
+
+    # Use full path to a file for TLS related configuration parameters
+    if [[ $var_name =~ ^TLS.*File$ ]]; then
+        var_value=$ZABBIX_USER_HOME_DIR/enc/$var_value
+    fi
+
+    # Escaping characters in parameter value and name
+    var_value=$(escape_spec_char "$var_value")
+    var_name=$(escape_spec_char "$var_name")
+
+    if [ "$(grep -E "^$var_name=" $config_path)" ] && [ "$is_multiple" != "true" ]; then
+        sed -i -e "/^$var_name=/s/=.*/=$var_value/" "$config_path"
+        echo "updated"
+    elif [ "$(grep -Ec "^# $var_name=" $config_path)" -gt 1 ]; then
+        sed -i -e  "/^[#;] $var_name=$/i\\$var_name=$var_value" "$config_path"
+        echo "added first occurrence"
+    elif [ "$(grep -Ec "^[#;] $var_name=" $config_path)" -gt 0 ]; then
+        sed -i -e "/^[#;] $var_name=/s/.*/&\n$var_name=$var_value/" "$config_path"
+        echo "added"
+    else
+        sed -i -e '$a\' -e "$var_name=$var_value" "$config_path"
+        echo "added at the end"
+    fi
+
+}
+
+update_config_multiple_var() {
+    local config_path=$1
+    local var_name=$2
+    local var_value=$3
+
+    var_value="${var_value%\"}"
+    var_value="${var_value#\"}"
+
+    local IFS=,
+    local OPT_LIST=($var_value)
+
+    for value in "${OPT_LIST[@]}"; do
+        update_config_var $config_path $var_name $value true
+    done
+}
+
+prepare_zbx_agent_config() {
+    echo "** Preparing Zabbix agent configuration file"
+    ZBX_AGENT_CONFIG=$ZABBIX_ETC_DIR/zabbix_agent2.conf
+
+    : ${ZBX_PASSIVESERVERS:=""}
+    : ${ZBX_ACTIVESERVERS:=""}
+
+    [ -n "$ZBX_PASSIVESERVERS" ] && ZBX_PASSIVESERVERS=","$ZBX_PASSIVESERVERS
+
+    ZBX_PASSIVESERVERS=$ZBX_SERVER_HOST$ZBX_PASSIVESERVERS
+
+    [ -n "$ZBX_ACTIVESERVERS" ] && ZBX_ACTIVESERVERS=","$ZBX_ACTIVESERVERS
+
+    ZBX_ACTIVESERVERS=$ZBX_SERVER_HOST":"$ZBX_SERVER_PORT$ZBX_ACTIVESERVERS
+
+    update_config_var $ZBX_AGENT_CONFIG "PidFile"
+    update_config_var $ZBX_AGENT_CONFIG "LogType" "console"
+    update_config_var $ZBX_AGENT_CONFIG "LogFile"
+    update_config_var $ZBX_AGENT_CONFIG "LogFileSize"
+    update_config_var $ZBX_AGENT_CONFIG "DebugLevel" "${ZBX_DEBUGLEVEL}"
+    update_config_var $ZBX_AGENT_CONFIG "SourceIP"
+    update_config_var $ZBX_AGENT_CONFIG "LogRemoteCommands" "${ZBX_LOGREMOTECOMMANDS}"
+
+    : ${ZBX_PASSIVE_ALLOW:="true"}
+    if [ "$ZBX_PASSIVE_ALLOW" == "true" ]; then
+        echo "** Using '$ZBX_PASSIVESERVERS' servers for passive checks"
+        update_config_var $ZBX_AGENT_CONFIG "Server" "${ZBX_PASSIVESERVERS}"
+    else
+        update_config_var $ZBX_AGENT_CONFIG "Server"
+    fi
+
+    update_config_var $ZBX_AGENT_CONFIG "ListenPort" "${ZBX_LISTENPORT}"
+    update_config_var $ZBX_AGENT_CONFIG "ListenIP" "${ZBX_LISTENIP}"
+
+    : ${ZBX_ACTIVE_ALLOW:="true"}
+    if [ "$ZBX_ACTIVE_ALLOW" == "true" ]; then
+        echo "** Using '$ZBX_ACTIVESERVERS' servers for active checks"
+        update_config_var $ZBX_AGENT_CONFIG "ServerActive" "${ZBX_ACTIVESERVERS}"
+    else
+        update_config_var $ZBX_AGENT_CONFIG "ServerActive"
+    fi
+
+    if [ "$ZBX_ENABLESTATUSPORT" == "true" ]; then
+        update_config_var $ZBX_AGENT_CONFIG "EnablePersistentBuffer" "1"
+        update_config_var $ZBX_AGENT_CONFIG "PersistentBufferFile" "$ZABBIX_USER_HOME_DIR/buffer/"
+        update_config_var $ZBX_AGENT_CONFIG "PersistentBufferPeriod" "${ZBX_PERSISTENTBUFFERPERIOD}"
+    else
+        update_config_var $ZBX_AGENT_CONFIG "EnablePersistentBuffer" "0"
+    fi
+
+    if [ "$ZBX_ENABLESTATUSPORT" == "true" ]; then
+        update_config_var $ZBX_AGENT_CONFIG "StatusPort" "31999"
+    fi
+
+#    update_config_var $ZBX_AGENT_CONFIG "HostInterface" "${ZBX_HOSTINTERFACE}"
+#    update_config_var $ZBX_AGENT_CONFIG "HostInterfaceItem" "${ZBX_HOSTINTERFACEITEM}"
+
+    update_config_var $ZBX_AGENT_CONFIG "Hostname" "${ZBX_HOSTNAME}"
+    update_config_var $ZBX_AGENT_CONFIG "HostnameItem" "${ZBX_HOSTNAMEITEM}"
+    update_config_var $ZBX_AGENT_CONFIG "HostMetadata" "${ZBX_METADATA}"
+    update_config_var $ZBX_AGENT_CONFIG "HostMetadataItem" "${ZBX_METADATAITEM}"
+    update_config_var $ZBX_AGENT_CONFIG "RefreshActiveChecks" "${ZBX_REFRESHACTIVECHECKS}"
+    update_config_var $ZBX_AGENT_CONFIG "BufferSend" "${ZBX_BUFFERSEND}"
+    update_config_var $ZBX_AGENT_CONFIG "BufferSize" "${ZBX_BUFFERSIZE}"
+    update_config_var $ZBX_AGENT_CONFIG "MaxLinesPerSecond" "${ZBX_MAXLINESPERSECOND}"
+    # Please use include to enable Alias feature
+#    update_config_multiple_var $ZBX_AGENT_CONFIG "Alias" ${ZBX_ALIAS}
+    update_config_var $ZBX_AGENT_CONFIG "Timeout" "${ZBX_TIMEOUT}"
+    update_config_var $ZBX_AGENT_CONFIG "Include" "/etc/zabbix/zabbix_agentd.d/"
+    update_config_var $ZBX_AGENT_CONFIG "UnsafeUserParameters" "${ZBX_UNSAFEUSERPARAMETERS}"
+    update_config_var $ZBX_AGENT_CONFIG "TLSConnect" "${ZBX_TLSCONNECT}"
+    update_config_var $ZBX_AGENT_CONFIG "TLSAccept" "${ZBX_TLSACCEPT}"
+    update_config_var $ZBX_AGENT_CONFIG "TLSCAFile" "${ZBX_TLSCAFILE}"
+    update_config_var $ZBX_AGENT_CONFIG "TLSCRLFile" "${ZBX_TLSCRLFILE}"
+    update_config_var $ZBX_AGENT_CONFIG "TLSServerCertIssuer" "${ZBX_TLSSERVERCERTISSUER}"
+    update_config_var $ZBX_AGENT_CONFIG "TLSServerCertSubject" "${ZBX_TLSSERVERCERTSUBJECT}"
+    update_config_var $ZBX_AGENT_CONFIG "TLSCertFile" "${ZBX_TLSCERTFILE}"
+    update_config_var $ZBX_AGENT_CONFIG "TLSKeyFile" "${ZBX_TLSKEYFILE}"
+    update_config_var $ZBX_AGENT_CONFIG "TLSPSKIdentity" "${ZBX_TLSPSKIDENTITY}"
+    update_config_var $ZBX_AGENT_CONFIG "TLSPSKFile" "${ZBX_TLSPSKFILE}"
+
+    update_config_multiple_var $ZBX_AGENT_CONFIG "DenyKey" "${ZBX_DENYKEY}"
+    update_config_multiple_var $ZBX_AGENT_CONFIG "AllowKey" "${ZBX_ALLOWKEY}"
+}
+
+prepare_agent() {
+    echo "** Preparing Zabbix agent"
+    prepare_zbx_agent_config
+}
+
+#################################################
+
+if [ "${1#-}" != "$1" ]; then
+    set -- /usr/sbin/zabbix_agent2 "$@"
+fi
+
+if [ "$1" == '/usr/sbin/zabbix_agent2' ]; then
+    prepare_agent
+fi
+
+exec "$@"
+
+#################################################
