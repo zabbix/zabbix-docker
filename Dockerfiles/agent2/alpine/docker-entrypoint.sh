d4f6f49465b7d3f9dee287e510cd6d34b20fe3c7
Alexey Pustovalov <alexey.pustovalov@zabbix.com>
2023-08-16 16:55:59 +0900

    Fixed missing and incorrect variables

--- Dockerfiles/agent2/alpine/docker-entrypoint.sh
+++ Dockerfiles/agent2/alpine/docker-entrypoint.sh
@@ -153,6 +153,7 @@ prepare_zbx_agent_config() {
         update_config_var $ZBX_AGENT_CONFIG "ServerActive"
     fi
     update_config_var $ZBX_AGENT_CONFIG "HeartbeatFrequency" "${ZBX_HEARTBEAT_FREQUENCY}"
+    update_config_var $ZBX_AGENT_CONFIG "ForceActiveChecksOnStart" "${ZBX_FORCEACTIVECHECKSONSTART}"
 
     if [ "${ZBX_ENABLEPERSISTENTBUFFER,,}" == "true" ]; then
         update_config_var $ZBX_AGENT_CONFIG "EnablePersistentBuffer" "1"
