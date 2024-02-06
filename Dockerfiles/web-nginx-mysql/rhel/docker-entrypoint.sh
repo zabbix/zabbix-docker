9e3c6efd1811a842ece18e266a743b5631c5f8a9
Alexey Pustovalov <alexey.pustovalov@zabbix.com>
2024-02-06 17:54:37 +0900

    Added EXPOSE_WEB_SERVER_INFO variable to control web server / php versions expose

--- Dockerfiles/web-nginx-mysql/rhel/docker-entrypoint.sh
+++ Dockerfiles/web-nginx-mysql/rhel/docker-entrypoint.sh
@@ -23,6 +23,8 @@ fi
 ZABBIX_ETC_DIR="/etc/zabbix"
 # Web interface www-root directory
 ZABBIX_WWW_ROOT="/usr/share/zabbix"
+# Nginx main configuration file
+NGINX_CONF_FILE="/etc/nginx/nginx.conf"
 
 # usage: file_env VAR [DEFAULT]
 # as example: file_env 'MYSQL_PASSWORD' 'zabbix'
@@ -254,14 +256,23 @@ prepare_zbx_web_config() {
     if [ "${ENABLE_WEB_ACCESS_LOG,,}" == "false" ]; then
         sed -ri \
             -e 's!^(\s*access_log).+\;!\1 off\;!g' \
-            "/etc/nginx/nginx.conf"
+            "$NGINX_CONF_FILE"
         sed -ri \
             -e 's!^(\s*access_log).+\;!\1 off\;!g' \
-            "/etc/zabbix/nginx.conf"
+            "$ZABBIX_ETC_DIR/nginx.conf"
         sed -ri \
             -e 's!^(\s*access_log).+\;!\1 off\;!g' \
-            "/etc/zabbix/nginx_ssl.conf"
+            "$ZABBIX_ETC_DIR/nginx_ssl.conf"
     fi
+
+    : ${EXPOSE_WEB_SERVER_INFO:="on"}
+
+    [[ "${EXPOSE_WEB_SERVER_INFO}" != "off" ]] && EXPOSE_WEB_SERVER_INFO="on"
+
+    export EXPOSE_WEB_SERVER_INFO=${EXPOSE_WEB_SERVER_INFO}
+    sed -i \
+        -e "s/{EXPOSE_WEB_SERVER_INFO}/${EXPOSE_WEB_SERVER_INFO}/g" \
+    "$NGINX_CONF_FILE"
 }
 
 #################################################
