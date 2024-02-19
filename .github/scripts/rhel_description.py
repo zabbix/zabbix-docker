d70750efb653eb296f411d3bd220d8dfbffdee46
Alexey Pustovalov <alexey.pustovalov@zabbix.com>
2024-02-20 00:42:03 +0900

    Added workflow to update Red Hat Container Catalog

--- .github/scripts/rhel_description.py
+++ .github/scripts/rhel_description.py
@@ -4,20 +4,32 @@ import json
 import markdown
 import os
 
-repository_description = ''
+repository_description = None
 
 if ("DESCRIPTION_FILE" not in os.environ):
     print("Description file environment variable is not specified")
     sys.exit(1)
+if ("PYXIS_API_TOKEN" not in os.environ):
+    print("API token environment variable is not specified")
+    sys.exit(1)
+if ("API_URL" not in os.environ):
+    print("API URL environment variable is not specified")
+    sys.exit(1)
+if ("PROJECT_ID" not in os.environ):
+    print("RedHat project ID environment variable is not specified")
+    sys.exit(1)
 
 if (os.path.isfile(os.environ["DESCRIPTION_FILE"] + '.md')):
-    repository_description=markdown.markdownFromFile(input=os.environ["DESCRIPTION_FILE"] + '.md')
+    file = open(os.environ["DESCRIPTION_FILE"] + '.md', mode='r')
+    markdown_data = file.read()
+    file.close()
+    repository_description=markdown.markdown(markdown_data)
 elif (os.path.isfile(os.environ["DESCRIPTION_FILE"] + '.html')):
     file = open(os.environ["DESCRIPTION_FILE"] + '.html', mode='r')
     repository_description = file.read()
     file.close()
 
-if (len(repository_description)) == 0:
+if (repository_description is None or len(repository_description) == 0):
     print("No description")
     sys.exit(1)
 
