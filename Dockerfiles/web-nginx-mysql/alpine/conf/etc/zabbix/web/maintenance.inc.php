e7bb4fef5a44983f896d5f61fcd664d219b4dcf2
Alexey Pustovalov <alexey.pustovalov@zabbix.com>
2025-07-14 19:16:22 +0900

    Fixed maintenance message for Web-interface

--- Dockerfiles/web-nginx-mysql/alpine/conf/etc/zabbix/web/maintenance.inc.php
+++ Dockerfiles/web-nginx-mysql/alpine/conf/etc/zabbix/web/maintenance.inc.php
@@ -1,32 +1,27 @@
 <?php
 /*
-** Zabbix
-** Copyright (C) 2001-2016 Zabbix SIA
+** Copyright (C) 2001-2025 Zabbix SIA
 **
-** This program is free software; you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 2 of the License, or
-** (at your option) any later version.
+** This program is free software: you can redistribute it and/or modify it under the terms of
+** the GNU Affero General Public License as published by the Free Software Foundation, version 3.
 **
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-** GNU General Public License for more details.
+** This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+** without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU Affero General Public License for more details.
 **
-** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software
-** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+** You should have received a copy of the GNU Affero General Public License along with this program.
+** If not, see <https://www.gnu.org/licenses/>.
 **/
 
 
-// Maintenance mode
+// Maintenance mode.
 if (getenv('ZBX_DENY_GUI_ACCESS') == 'true') {
     define('ZBX_DENY_GUI_ACCESS', 1);
 
-    // IP range, who are allowed to connect to FrontEnd
+    // Array of IP addresses, which are allowed to connect to frontend (optional).
     $ip_range = str_replace("'","\"",getenv('ZBX_GUI_ACCESS_IP_RANGE'));
     $ZBX_GUI_ACCESS_IP_RANGE = (json_decode($ip_range)) ? json_decode($ip_range, true) : array();
 
-    // MSG shown on Warning screen!
-    $_REQUEST['warning_msg'] = getenv('ZBX_GUI_WARNING_MSG');
+    // Message shown on warning screen (optional).
+    $ZBX_GUI_ACCESS_MESSAGE = getenv('ZBX_GUI_WARNING_MSG');
 }
