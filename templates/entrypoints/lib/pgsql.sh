9c9711da60cda390d21797e0770d2c6a56e2bdfc
Edgars Melveris <edgars.melveris@zabbix.com>
2026-05-11 05:51:50 +0300

    sanitize vault url, prefix and dbpath

--- templates/entrypoints/lib/pgsql.sh
+++ templates/entrypoints/lib/pgsql.sh
@@ -58,12 +58,22 @@ get_vault_secrets() {
         error "Missing variables! If ZBX_VAULT is used then ZBX_VAULTURL and ZBX_VAULTDBPATH must be set"
     fi
 
+    # Sanitize input
+    ZBX_VAULTURL="${ZBX_VAULTURL#/}"
+    ZBX_VAULTURL="${ZBX_VAULTURL%/}/"
+    ZBX_VAULTDBPATH="${ZBX_VAULTDBPATH#/}"
+    ZBX_VAULTDBPATH="${ZBX_VAULTDBPATH%/}"
+
     if [ "${ZBX_VAULT:-}" = "HashiCorp" ]; then
         if [ -z "$ZBX_VAULTPREFIX" ]; then
             ZBX_VAULTPREFIX="/v1/${ZBX_VAULTDBPATH%/*}/data/"
             ZBX_VAULTDBPATH="${ZBX_VAULTDBPATH##*/}"
+        else
+            ZBX_VAULTPREFIX="${ZBX_VAULTPREFIX#/}"
+            ZBX_VAULTPREFIX="${ZBX_VAULTPREFIX%/}/"
         fi
         local vault_url="${ZBX_VAULTURL}${ZBX_VAULTPREFIX}${ZBX_VAULTDBPATH}"
+        info "***** VAULT URL: $vault_url"
         while ! vaultdata="$(curl "${curl_opts[@]}" -H "X-Vault-Token: $VAULT_TOKEN" "$vault_url")"; do
             info "**** Vault is not available. Waiting ${wait_timeout} seconds... ****"
             sleep "$wait_timeout"
@@ -76,11 +86,18 @@ get_vault_secrets() {
         DB_SERVER_ZBX_PASS="$(printf '%s' "$vaultdata" | jq -r '.data.data.password')"
 
     elif [ "${ZBX_VAULT:-}" = "CyberArk" ]; then
+        if [ -z "${ZBX_VAULTCERTFILE:-}" ] ; then
+            error "Missing variables! If CyberArk is used then ZBX_VAULTCERTFILE must be set"
+        fi
         cyberark_opts=(-H "Content-type: application/json" --cert "$ZBX_VAULTCERTFILE")
         if [ -z "$ZBX_VAULTPREFIX" ]; then
             ZBX_VAULTPREFIX="/AIMWebService/api/Accounts?"
+        else
+            ZBX_VAULTPREFIX="${ZBX_VAULTPREFIX#/}"
+            ZBX_VAULTPREFIX="${ZBX_VAULTPREFIX%/}/"
         fi
         local vault_url="${ZBX_VAULTURL}${ZBX_VAULTPREFIX}${ZBX_VAULTDBPATH}"
+        info "***** VAULT URL: $vault_url"
         # if key is defined use it
         if [ -n "${ZBX_VAULTKEYFILE:-}" ]; then
             cyberark_opts+=(--key "$ZBX_VAULTKEYFILE")
@@ -128,7 +145,6 @@ check_db_connect() {
         unset DB_SERVER_ZBX_PASS
 
         info "***** Connecting to vault... ******"
-        info "***** VAULT URL: $ZBX_VAULTURL"
         get_vault_secrets
     fi
 
