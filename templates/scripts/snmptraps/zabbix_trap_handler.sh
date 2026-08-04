861408b9a8d4b16cda8989f4c285e3efe4299fbd
Alexey Pustovalov <alexey.pustovalov@zabbix.com>
2026-08-04 14:18:29 +0900

    More precise checking for SNMP traps injections

--- templates/scripts/snmptraps/zabbix_trap_handler.sh
+++ templates/scripts/snmptraps/zabbix_trap_handler.sh
@@ -10,6 +10,20 @@ ZBX_SNMP_TRAP_USE_DNS="${ZBX_SNMP_TRAP_USE_DNS:-false}"
 
 date_now="$(date "$ZBX_SNMP_TRAP_DATE_FORMAT")"
 
+date_regex="$(
+    printf '%s' "$ZBX_SNMP_TRAP_DATE_FORMAT" | sed \
+        -e 's/^+//g' \
+        -e 's/%Y/[0-9]\{4\}/g' \
+        -e 's/%m/[0-9]\{2\}/g' \
+        -e 's/%d/[0-9]\{2\}/g' \
+        -e 's/%T/[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}/g' \
+        -e 's/%z/[\+\-][0-9]\{4\}/g' \
+        -e 's/%H/[0-9]\{2\}/g' \
+        -e 's/%M/[0-9]\{2\}/g' \
+        -e 's/%S/[0-9]\{2\}/g'
+)"
+zbx_trap_regex="${date_regex} ZBXTRAP"
+
 trap_address=""
 sender_addr=""
 sender_regex='\[([^]]+)\].*->'
@@ -29,14 +43,19 @@ IFS= read -r sender
 
 # The remaining lines will contain the payload varbind list. For SNMPv1 traps, the final OID will be SNMPv2-MIB::snmpTrapEnterprise.0.
 while read -r oid val; do
+    # Header in Zabbix format shouldn't exist anywhere in vars, it is injection
+    # Must exit with 0
+    [[ "$val" =~ $zbx_trap_regex ]] && exit 0
+    sanitized_val=${val//ZBXTRAP/\'ZBXTRAP\'}
+
     if [ -z "$vars" ]; then
-        vars="$oid = $val"
+        vars="$oid = $sanitized_val"
     else
-        vars="${vars}${ZBX_SNMP_TRAP_FORMAT}${oid} = $val"
+        vars="${vars}${ZBX_SNMP_TRAP_FORMAT}${oid} = $sanitized_val"
     fi
 
     if [[ "$oid" =~ snmpTrapAddress\.0 ]] || [[ "$oid" =~ 1\.3\.6\.1\.6\.3\.18\.1\.3\.0 ]]; then
-        trap_address="$val"
+        trap_address="$sanitized_val"
     fi
 done
 
@@ -51,23 +70,5 @@ fi
 if [[ "$ZBX_SNMP_TRAP_USE_DNS" == "true" ]] && ! [[ "${host:-}" =~ $sender_regex ]]; then
     sender_addr="$host"
 fi
-
-# Header in Zabbix format shouldn't exist anywhere in vars, it is injection
-# Must exit with 0
-date_regex="$(
-    printf '%s' "$ZBX_SNMP_TRAP_DATE_FORMAT" | sed \
-        -e 's/^+//g' \
-        -e 's/%Y/[0-9]\{4\}/g' \
-        -e 's/%m/[0-9]\{2\}/g' \
-        -e 's/%d/[0-9]\{2\}/g' \
-        -e 's/%T/[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}/g' \
-        -e 's/%z/[\+\-][0-9]\{4\}/g' \
-        -e 's/%H/[0-9]\{2\}/g' \
-        -e 's/%M/[0-9]\{2\}/g' \
-        -e 's/%S/[0-9]\{2\}/g'
-)"
-
-zbx_trap_regex="${date_regex} ZBXTRAP"
-printf '%s\n' "$vars" | grep -qE "$zbx_trap_regex" && exit 0
 
 printf '%b\n' "${date_now} ZBXTRAP ${sender_addr}${ZBX_SNMP_TRAP_FORMAT}${sender}${ZBX_SNMP_TRAP_FORMAT}${vars}" >> "$ZABBIX_TRAPS_FILE"
