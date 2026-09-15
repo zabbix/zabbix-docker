8055e80f0ccb61fcbe0b5bb0d8a0108e09a3b453
Alexey Pustovalov <alexey.pustovalov@zabbix.com>
2026-09-15 14:30:10 +0900

    Fixed OpenSSL rehash function

--- templates/entrypoints/lib/openssl.sh
+++ templates/entrypoints/lib/openssl.sh
@@ -1,11 +1,18 @@
 # shellcheck shell=bash
 
 openssl_rehash() {
-    local ssl_location="${1:-}"
+    local ssl_location="$1"
 
-    if command -v openssl >/dev/null 2>&1 && [ -n "$ssl_location" ] && [ -d "$ssl_location" ]; then
-        if ! openssl rehash "$ssl_location" >/dev/null; then
-            warn "openssl rehash failed for '$ssl_location'"
-        fi
+    if ! openssl rehash "$ssl_location" >/dev/null; then
+        warn "openssl rehash failed for '$ssl_location'"
     fi
+}
+
+openssl_prepare_ca() {
+    local source_dir="$1"
+    local target_dir="$2"
+
+    rm -rf "$target_dir"/*
+    cp -a "$source_dir/." "$target_dir/"
+    openssl_rehash "$target_dir"
 }
